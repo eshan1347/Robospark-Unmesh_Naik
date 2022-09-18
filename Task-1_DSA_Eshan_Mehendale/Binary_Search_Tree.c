e87bbb5b4d1e74f0658eae3bbcf81c46119a5219
eshan1347 <eshan.mehendale21@vit.edu>
2022-09-18 23:51:35 +0530

    task

---
+++ Task-1_DSA_Eshan_Mehendale/Binary_Search_Tree.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int key;
+  struct node *left, *right;
+};
+
+struct node *newNode(int item) {
+  struct node *temp = (struct node *)malloc(sizeof(struct node));
+  temp->key = item;
+  temp->left = temp->right = NULL;
+  return temp;
+}
+
+void Seq(struct node *root) {
+  if (root != NULL) {
+    
+    Seq(root->left);
+
+    
+    printf("%d -> ", root->key);
+
+    Seq(root->right);
+  }
+}
+
+
+struct node *insert(struct node *node, int key) {
+  
+  if (node == NULL) return newNode(key);
+
+  
+  if (key < node->key)
+    node->left = insert(node->left, key);
+  else
+    node->right = insert(node->right, key);
+
+  return node;
+}
+
+
+struct node *min(struct node *node) {
+  struct node *current = node;
+
+  
+  while (current && current->left != NULL)
+    current = current->left;
+
+  return current;
+}
+
+
+struct node *deleteNode(struct node *root, int key) {
+  
+  if (root == NULL) return root;
+
+ 
+  if (key < root->key)
+    root->left = deleteNode(root->left, key);
+  else if (key > root->key)
+    root->right = deleteNode(root->right, key);
+
+  else {
+    
+    if (root->left == NULL) {
+      struct node *temp = root->right;
+      free(root);
+      return temp;
+    } else if (root->right == NULL) {
+      struct node *temp = root->left;
+      free(root);
+      return temp;
+    }
+
+    
+    struct node *temp = min(root->right);
+
+    
+    root->key = temp->key;
+
+    
+    root->right = deleteNode(root->right, temp->key);
+  }
+  return root;
+}
+
+
+int main() {
+  struct node *root = NULL;
+  root = insert(root, 8);
+  root = insert(root, 3);
+  root = insert(root, 1);
+  root = insert(root, 6);
+  root = insert(root, 7);
+  root = insert(root, 10);
+  root = insert(root, 14);
+  root = insert(root, 4);
+
+  printf("ISequential traversal: ");
+  Seq(root);
+
+  printf("\nAfter deleting 10\n");
+  root = deleteNode(root, 10);
+  printf("Sequential traversal: ");
+  Seq(root);
+}
